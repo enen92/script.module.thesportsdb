5b59230fb040a5f84d95440e682954ab9a536c31
enen92 <92enen@gmail.com>
2015-02-10 22:14:09 +0000

    sync

--- lib/thesportsdb/api.py
+++ lib/thesportsdb/api.py
@@ -121,8 +121,9 @@ class Schedules:
 		data = json.load(urllib2.urlopen(url))
 		return data
 		
-	def eventspasttleague(self,LeagueId):
-		url = '%s/%s/eventspasttleague.php?id=%s' % (API_BASE_URL,API_KEY,str(LeagueId))
+	def eventspastleague(self,LeagueId):
+		url = '%s/%s/eventspastleague.php?id=%s' % (API_BASE_URL,API_KEY,str(LeagueId))
+		print url
 		data = json.load(urllib2.urlopen(url))
 		return data
 
